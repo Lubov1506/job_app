3ec3266f89cecbb6422a11c56213619a933124ba
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2026-03-20 15:25:09 +0200

    proxy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,10 @@
-export default function Dashboard (){
-    return <p>Dashboard</p>
+import { getSession } from "@/lib/auth/auth"
+import { redirect } from "next/navigation"
+
+export default async function Dashboard() {
+  const session = await getSession()
+  if (!session?.user) {
+     redirect("/sign-in")
+  }
+  return <p>Dashboard</p>
 }
